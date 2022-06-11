9b0d907eb9533c578bedb90b36420b20565e605d
Khushi <khushicoding1101@gmail.com>
2022-06-11 23:07:55 +0530

    first commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("contact", views.contact, name='contact'),
+    path("other", views.other, name='other'),
+    path("college", views.college, name='college'),
+    path("course", views.course, name='course'),
+    path("company", views.company, name='company'),
+] 
---
+++ home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, "about.html")
+def contact(request):
+    if request.method == "POST":
+        Name = request.POST.get('Name')
+        Phone = request.POST.get('Phone')
+        Email = request.POST.get('Email')
+        contact = Contact(Name=Name, Email=Email, Phone=Phone)
+        contact.save()
+        messages.success(request, 'Your profile details updated!')
+    return render(request, "contact.html")
+def other(request):
+    return render(request, "other.html")
+def college(request):
+    return render(request, "college.html")
+def course(request):
+    return render(request, "course.html")    
+def company(request):
+    return render(request, "company.html")
